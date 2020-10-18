3d906b390acb9af0bbc7c459db7fd4d53d85ace7
Zinzin92 <kevin-descours@outlook.fr>
2020-10-18 20:52:01 +0200

    Update EyPluginComponent.php

--- app/Controller/Component/EyPluginComponent.php
+++ app/Controller/Component/EyPluginComponent.php
@@ -10,7 +10,7 @@ class EyPluginComponent extends CakeObject
     public $pluginsInFolder = array();
     public $pluginsInDB = array();
     private $alreadyCheckValid = array();
-    private $reference = 'https://raw.githubusercontent.com/MineWeb/mineweb.org/gh-pages/market/plugins.json';
+    private $reference = 'https://raw.githubusercontent.com/zinzin92/fivem-cms.org/gh-pages/market/plugins.json';
 
     public $pluginsFolder;
 
